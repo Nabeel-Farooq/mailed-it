3414614886b09fd7004779e92d3f3a22cc318fca
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-06-22 14:24:19 +0500

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from app.models import Subscriber
-from app.models import Campaign
 
-admin.site.register(Subscriber)
-admin.site.register(Campaign)
+from app.models import Campaign, Subscriber
+
+
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ("id",)
+    search_fields = ("id",)
+    ordering = ("id",)
+
+
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = ("id",)
+    search_fields = ("id",)
+    ordering = ("id",)
