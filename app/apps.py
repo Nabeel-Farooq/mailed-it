1e4b481889e6d10f680cf310e2f383e532a1ddb3
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-06-22 14:24:35 +0500

    Update apps.py

--- app/apps.py
+++ app/apps.py
@@ -1,6 +1,7 @@
-from django.apps import AppConfig
+from django.apps import AppConfig as DjangoAppConfig
 
 
-class AppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'
+class AppConfig(DjangoAppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "app"
+    verbose_name = "Application"
