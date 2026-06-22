90258f3c5145b7330fc423b483963681eeb93e19
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-06-22 14:25:14 +0500

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,17 +1,30 @@
-from django.contrib import admin
-from django.urls import path 
+from django.urls import path
+
 from app import views
 
 
+app_name = "app"
+
 urlpatterns = [
-    path("",views.main, name='Home'),
-    path("add_subscribe",views.add_subscribe, name='add_subscribe'),
-    path("unsubscribe",views.unsubscribe, name='unsubscribe'),
-    path("add_campaign",views.add_campaign, name='add_campaign'),
-    path("send_mail",views.send_mail, name='send_mail'),
-    path("success_page",views.success_page, name='success_page'),
-    path("import_subscribers",views.import_subscribers, name='import_subscribers'),
-    path("user_unsubscribe",views.user_unsubscribe,name="user_unsubscribe"),
-    path("upload_template",views.upload_template,name='upload_template')
+    path("", views.main, name="home"),
+    path("add-subscribe/", views.add_subscribe, name="add_subscribe"),
+    path("unsubscribe/", views.unsubscribe, name="unsubscribe"),
+    path("add-campaign/", views.add_campaign, name="add_campaign"),
+    path("send-mail/", views.send_mail, name="send_mail"),
+    path("success/", views.success_page, name="success_page"),
+    path(
+        "import-subscribers/",
+        views.import_subscribers,
+        name="import_subscribers",
+    ),
+    path(
+        "user-unsubscribe/",
+        views.user_unsubscribe,
+        name="user_unsubscribe",
+    ),
+    path(
+        "upload-template/",
+        views.upload_template,
+        name="upload_template",
+    ),
 ]
-    
